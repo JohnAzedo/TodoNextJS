9746b55785eea8bc3820f4b9ad5fc516c7ee1686
JohnAzedo <jplimao12@gmail.com>
2021-02-17 18:25:02 -0300

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-
+import "bootstrap/dist/css/bootstrap-grid.min.css"
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="d-flex">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
 export default MyApp
